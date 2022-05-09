cb2f1eaa7ca977ac11f4f1b693fad05de1c140e6
Fachri Luthfiansyah <fahrilut12@gmail.com>
2022-05-09 21:51:54 +0700

    pengerjaan order Page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import "./Styles/App.css";
 import React from "react";
 import Login from "./View/login.jsx";
 import Register from "./View/register.jsx";
-import Home from "./View/homePage";
+import Home from "./View/homePage.jsx";
 import DetailProduct from "./View/detail.jsx";
+import Order from "./View/orderPage.jsx";
 
 function App() {
   return (
     <div className="App">
       {/* <Login /> */}
       {/* <Register /> */}
-      <Home />
+      {/* <Home /> */}
+      <Order />
       {/* <DetailProduct /> */}
     </div>
   );
