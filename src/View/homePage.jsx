02fdc92ae1b090dec97f72179ffbae1678eff9ff
Fachri Luthfiansyah <fahrilut12@gmail.com>
2022-05-11 14:18:39 +0700

    update homePage

--- src/View/homePage.jsx
+++ src/View/homePage.jsx
@@ -23,18 +23,8 @@ function Homepage() {
 
       <div className="background">
         <div className="row">
-          <div className="col-lg-3 col-md-3 sol-sm-3 col-3">
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={Logo} />
-              <Card.Body>
-                <Card.Title className="card-title">Product Title</Card.Title>
-                <Card.Text className="card-text">$45</Card.Text>
-                <Button className="button-style">Add to cart</Button>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="col-lg-3 col-md-3 sol-sm-3 col-3">
-            <Card style={{ width: "18rem" }}>
+          <div className="col-lg-3 col-md-3 sol-sm-3 col-3 ">
+            <Card style={{ width: "18rem" }} className="shadow-card">
               <Card.Img variant="top" src={Logo} />
               <Card.Body>
                 <Card.Title className="card-title">Product Title</Card.Title>
@@ -44,7 +34,7 @@ function Homepage() {
             </Card>
           </div>
           <div className="col-lg-3 col-md-3 sol-sm-3 col-3">
-            <Card style={{ width: "18rem" }}>
+            <Card style={{ width: "18rem" }} className="shadow-card">
               <Card.Img variant="top" src={Logo} />
               <Card.Body>
                 <Card.Title className="card-title">Product Title</Card.Title>
@@ -54,7 +44,7 @@ function Homepage() {
             </Card>
           </div>
           <div className="col-lg-3 col-md-3 sol-sm-3 col-3">
-            <Card style={{ width: "18rem" }}>
+            <Card style={{ width: "18rem" }} className="shadow-card">
               <Card.Img variant="top" src={Logo} />
               <Card.Body>
                 <Card.Title className="card-title">Product Title</Card.Title>
@@ -63,8 +53,8 @@ function Homepage() {
               </Card.Body>
             </Card>
           </div>
-          <div className="col-lg-3 col-md-3 sol-sm-3 col-3 ">
-            <Card style={{ width: "18rem" }}>
+          <div className="col-lg-3 col-md-3 sol-sm-3 col-3">
+            <Card style={{ width: "18rem" }} className="shadow-card">
               <Card.Img variant="top" src={Logo} />
               <Card.Body>
                 <Card.Title className="card-title">Product Title</Card.Title>
@@ -73,8 +63,8 @@ function Homepage() {
               </Card.Body>
             </Card>
           </div>
-          <div className="col-lg-3 col-md-3 sol-sm-3 col-3 ">
-            <Card style={{ width: "18rem" }}>
+          <div className="col-lg-3 col-md-3 sol-sm-3 col-3 mt-5">
+            <Card style={{ width: "18rem" }} className="shadow-card">
               <Card.Img variant="top" src={Logo} />
               <Card.Body>
                 <Card.Title className="card-title">Product Title</Card.Title>
@@ -83,8 +73,8 @@ function Homepage() {
               </Card.Body>
             </Card>
           </div>
-          <div className="col-lg-3 col-md-3 sol-sm-3 col-3 ">
-            <Card style={{ width: "18rem" }}>
+          <div className="col-lg-3 col-md-3 sol-sm-3 col-3 mt-5">
+            <Card style={{ width: "18rem" }} className="shadow-card">
               <Card.Img variant="top" src={Logo} />
               <Card.Body>
                 <Card.Title className="card-title">Product Title</Card.Title>
@@ -93,8 +83,18 @@ function Homepage() {
               </Card.Body>
             </Card>
           </div>
-          <div className="col-lg-3 col-md-3 sol-sm-3 col-3 ">
-            <Card style={{ width: "18rem" }}>
+          <div className="col-lg-3 col-md-3 sol-sm-3 col-3 mt-5 ">
+            <Card style={{ width: "18rem" }} className="shadow-card">
+              <Card.Img variant="top" src={Logo} />
+              <Card.Body>
+                <Card.Title className="card-title">Product Title</Card.Title>
+                <Card.Text className="card-text">$45</Card.Text>
+                <Button className="button-style">Add to cart</Button>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col-lg-3 col-md-3 sol-sm-3 col-3 mt-5">
+            <Card style={{ width: "18rem" }} className="shadow-card">
               <Card.Img variant="top" src={Logo} />
               <Card.Body>
                 <Card.Title className="card-title">Product Title</Card.Title>
@@ -105,8 +105,6 @@ function Homepage() {
           </div>
         </div>
       </div>
-
-      
 
       {/* footer */}
       <div className="bg-footer">
